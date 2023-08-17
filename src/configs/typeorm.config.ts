a6efdea40a03615fd8eb9105c757ea1c46b10222
studio-kyj <studio.BE.YJ@gmail.com>
2023-08-17 17:41:17 +0900

    feat : config 생성

---
+++ src/configs/typeorm.config.ts
@@ -0,0 +1,17 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+export const typeORMConfig = (
+  configService: ConfigService,
+): TypeOrmModuleOptions => {
+  return {
+    type: 'postgres',
+    host: configService.get<string>('DB_HOST', 'localhost'),
+    port: configService.get<number>('DB_PORT'),
+    username: configService.get<string>('DB_USERNAME'), // 로컬 db
+    password: configService.get<string>('DB_PASSWORD'),
+    database: 'postgres', //로컬 db
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
+    synchronize: configService.get<boolean>('DB_SYNCHRONIZE'),
+  };
+};
