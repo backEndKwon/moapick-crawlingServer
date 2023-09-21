2b64defad877e977745387767eb64e46d2218926
minseok1109 <park485201@naver.com>
2023-09-21 18:14:29 +0900

    feat: 사람인 보류 & 프로그래머스 로그인 체크 추가

---
+++ src/crawling/saramin/checkSaraminLogin.ts
@@ -0,0 +1,49 @@
+import { chromium } from "playwright";
+
+const buttonSelector = {
+  idInput: "input[name='id']",
+  passwordInput: "input[name='password']",
+  submitButton: 'button:has-text("로그인")',
+  companyButton: "button:has-text('기업회원')",
+};
+
+export async function saraminLoginCheck(ID: string, PW: string) {
+  const browser = await chromium.launch({
+    headless: false,
+  });
+  const userAgent =
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36";
+  const context = await browser.newContext({ userAgent });
+  context.setDefaultNavigationTimeout(0);
+  context.setDefaultTimeout(0);
+
+  const page = await context.newPage();
+  try {
+    await page.goto("https://www.saramin.co.kr/zf_user/auth");
+    const companyButton = await page.waitForSelector(
+      buttonSelector.companyButton,
+    );
+
+    await companyButton.click();
+
+    await (await page.waitForSelector(buttonSelector.idInput)).fill(ID);
+
+    await (await page.waitForSelector(buttonSelector.passwordInput)).fill(PW);
+    await (await page.waitForSelector(buttonSelector.submitButton)).click();
+
+    const mainUrl = "https://www.saramin.co.kr/zf_user/memcom/index/main";
+
+    if ((await page.url()) !== mainUrl) {
+      console.log(await page.url());
+      throw new Error("로그인 실패");
+    }
+
+    console.log("로그인 성공");
+    await browser.close();
+    return true;
+  } catch (error) {
+    console.log(error);
+    await browser.close();
+    return false;
+  }
+}
